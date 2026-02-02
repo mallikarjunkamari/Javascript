fac0fc9955db09ae8c104b703b2f28aaacc4f622
mallikarjun <mallukamari123@gmail.com>
2026-02-02 23:27:38 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,87 @@
+// console.log('Hello, World!');
+
+const { count } = require("console");
+const { compose } = require("stream");
+
+// let a = 5;
+// const val= 10;
+// var n = 15;
+// console.log(a + n  +val);
+// console.log(val);
+// console.log(n);
+
+// const pi = 3.142722;
+// let r = 4;
+// let res = 4*pi*r*r;
+// console.log(`Total area of the circle is ` + res);
+
+// let fname = "mallikarjun";
+// let lname = "Kamari";
+// console.log(fname + " " + lname);
+
+// let age = 25;
+// const country = "India";
+// var isStudent = true;
+
+// console.log("Age: " + age);
+// console.log("Country: " + country);
+// console.log("isStudent: " + isStudent);
+
+// let score = 10;
+// console.log("Initial Score: " + score);
+
+// score = 20;
+// console.log("Updated score: " + score);
+
+
+// function showScope(){
+//     let fname = "Mallikarjun";
+//     const county= "India";
+
+//     console.log("Name: " + fname);
+//     console.log("Country: " + county);
+// }
+// showScope()
+
+// let a = 15;
+// let b = 5;
+// console.log("Addotion: " + (a+b));
+// console.log("Substraction: " + (a-b));
+// console.log("Multiplication: " + (a*b));
+// console.log("Division: " + (a/b));
+// console.log("Inecrement: " + a++);
+// console.log("a= " + a);
+// console.log(2**3);
+
+// res = 5>3? "5 is greater than 3" : "3 is greater than 5";
+// console.log(res);
+
+// let age = 18;
+// let result = (age>=18)? "Eligible for vote" : "Not eligible for vote";
+// console.log(result);
+
+// function greet() {
+//     console.log("Hello Ajay Good night");
+// }
+// greet();
+
+// function greet(Name) {
+//     console.log("Hello " + Name + " Good night");
+// }
+// greet("Mallikarjun");
+
+
+// function Date1() {
+//     return new Date().getFullYear();
+// }
+
+// let year = Date1();
+// console.log(year);
+
+// function FullName(fname,lname){
+//     return (fname + " " + lname);
+// }
+// console.log("Mallikarjun","Kamari");
+
+let fname = "Mallikarjun";
+console.log(`My name is, ${fname}`);
